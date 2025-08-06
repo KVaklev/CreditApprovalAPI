3469358eabd8bf43799e69e1482718152b89a496
Kristian Vaklev <kristian.vaklev@prologistik.com>
2025-08-06 22:59:23 +0300

    Implementation of the FluentValidation while sticking to the validation rules in the assignment

---
+++ CreditApprovalAPI/Validators/CreditRequestCreateValidator.cs
@@ -0,0 +1,44 @@
+﻿using CreditApprovalAPI.DTOs;
+using CreditApprovalAPI.Enums;
+using FluentValidation;
+
+namespace CreditApprovalAPI.Validators
+{
+    public class CreditRequestCreateValidator : AbstractValidator<CreditRequestCreateDto>
+    {
+        public CreditRequestCreateValidator()
+        {
+            RuleFor(x => x.MonthlyIncome)
+                .GreaterThan(0)
+                .WithMessage("Monthly income must be greater than zero.");
+
+            RuleFor(x => x.CreditAmount)
+                .GreaterThan(0)
+                .WithMessage("Credit amount must be greater than zero.");
+
+            RuleFor(x => x.Email)
+                .NotEmpty()
+                .WithMessage("Email is required.")
+                .EmailAddress()
+                .WithMessage("Invalid email format.");
+
+            RuleFor(x => x.CreditType)
+                .IsInEnum()
+                .WithMessage("Invalid credit type specified.");
+
+            RuleFor(x => x)
+                .Must(x =>
+                {
+                    return x.CreditType switch
+                    {
+                        CreditType.Mortgage => x.CreditAmount <= 500000,
+                        CreditType.Auto => x.CreditAmount <= 50000,
+                        CreditType.Personal => x.CreditAmount <= 10000,
+                        _=> false
+                    };
+
+                });
+
+        }
+    }
+}
