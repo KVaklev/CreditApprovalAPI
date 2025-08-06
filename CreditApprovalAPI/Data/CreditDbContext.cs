6470a96ffdf419d538ee3afaa1519d954f16e79f
Kristian Vaklev <kristian.vaklev@prologistik.com>
2025-08-06 21:52:16 +0300

    Implementation of the CreditRequest model , enumerations for CreditType and CretidStatus  along with CreditDbContext

---
+++ CreditApprovalAPI/Data/CreditDbContext.cs
@@ -0,0 +1,22 @@
+﻿using CreditApprovalAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CreditApprovalAPI.Data
+{
+    public class CreditDbContext : DbContext
+    {
+        public CreditDbContext(DbContextOptions<CreditDbContext> options)
+            : base(options) { }
+
+        public DbSet<CreditRequest> CreditRequests { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CreditRequest>()
+                .HasIndex(r => r.RequestNumber)
+                .IsUnique();
+        }
+    }
+}
+
+
